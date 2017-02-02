14b5793ffea9ec3875e2627359766b3d748434b6
fomichev <w.dmitrij.fomichev@gmail.com>
2017-02-02 12:46:23 +0200

    fix

--- app/code/local/Stableflow/Pricelists/sql/pricelists_setup/install-0.2.0.php
+++ app/code/local/Stableflow/Pricelists/sql/pricelists_setup/install-0.2.0.php
@@ -4,7 +4,6 @@ $installer = $this;
 $installer->startSetup();
 
 
-
 $installer->run("
 CREATE TABLE `{$this->getTable('pricelists/pricelist')}` (
  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -17,16 +16,5 @@ CREATE TABLE `{$this->getTable('pricelists/pricelist')}` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 ");
 
-$installer->run("
-CREATE TABLE `{$this->getTable('pricelists/pricelist')}` (
- `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
- `id_company` int(10) NOT NULL,
- `filename` varchar(255) NOT NULL,
- `date` date NOT NULL,
- `column_price` char(1) NOT NULL,
- `column`
- PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
-");
  
 $installer->endSetup();
