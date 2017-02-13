1483d4654b57c8ac883bdc09b14509b9ea14f7b0
fomichev <w.dmitrij.fomichev@gmail.com>
2017-02-13 19:42:27 +0200

    download file name translit

--- app/code/local/Stableflow/Pricelists/controllers/Adminhtml/PricelistsController.php
+++ app/code/local/Stableflow/Pricelists/controllers/Adminhtml/PricelistsController.php
@@ -50,15 +50,20 @@ class Stableflow_Pricelists_Adminhtml_PricelistsController extends Mage_Adminhtm
                 try {
                     $path = Mage::getBaseDir('media') . DS . 'pricelists' . DS;
                     $filename = $_FILES['file']['name'];
+
+                    $rus = array('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' ');
+                    $lat = array('A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', 'y', 'y', 'y', 'e', 'yu', 'ya', '_');
+                    $filename = str_replace($rus, $lat, $filename);
+
                     $uploader = new Varien_File_Uploader('file');
                     $uploader->setAllowedExtensions(array('XLS','xls'));
                     $uploader->setAllowCreateFolders(true);
                     $uploader->setAllowRenameFiles(false);
                     $uploader->setFilesDispersion(false);
-                    $fileInfo = $uploader->save($path, $filename);
+                    $uploader->save($path, $filename);
 
                     $pricelist = new Stableflow_Pricelists_Model_Pricelist();
-                    $pricelist->setFilename($fileInfo['file']);
+                    $pricelist->setFilename($filename);
                     $pricelist->setDate('NOW');
                     $pricelist->setStatus(Stableflow_Pricelists_Model_Resource_Pricelist::STATUS_NOT_APPROVED);
                     $pricelist->setIdCompany($this->getRequest()->getParam('id_company'));
